684bc902fc02223e02b7f881cf4598454a9de379
Pablo <contactoemail.pablo@gmail.com>
2022-06-22 13:19:35 +0200

    setup 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
-      <h1>Hello</h1>
+      <h1>Kodak App</h1>
       </main>
 
     </div>
