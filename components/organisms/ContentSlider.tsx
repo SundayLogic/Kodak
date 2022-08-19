3c517315ad85c69ad8dab747ba28170d3a6b4a64
Pablo <contactoemail.pablo@gmail.com>
2022-08-19 14:11:29 +0200

    Fixing phone view 1.3

--- components/organisms/ContentSlider.tsx
+++ components/organisms/ContentSlider.tsx
@@ -9,9 +9,9 @@ interface ContentSliderProps {
 
 const ContentSlider = ({ contentResults, sliderName }: ContentSliderProps) => {
   return (
-    <section className="mt-2 pb-12 px-3 pt-3  h-[45vh] md:h-[45vh] lg:h-[55vh] flex flex-col space-y-3  group ">
+    <section className="mt-2 pb-12 px-3 pt-3  h-[48vh] md:h-[45vh] lg:h-[55vh] flex flex-col space-y-3  group">
       <TextAtom text={sliderName} type={"SECTION"} />
-      <div className="flex pl-5 items-center overflow-x-scroll bg-kodakPurple-10/5 lg:space-x-5 md:space-x-4 space-x-3 scrollbar-hide h-[35vh] md:h-[45vh] lg:h-[55vh]">
+      <div className="flex pl-5 items-center overflow-x-scroll bg-kodakPurple-10/5 lg:space-x-5 md:space-x-4 space-x-3 scrollbar-hide h-[40vh] md:h-[45vh] lg:h-[55vh]">
         {contentResults?.map((e: Serie | Movie) => (
           <ContentThumbnail
             image={e.poster_path}
