1354481bdb069ee07dca12ec5ffa000a738674aa
Pablo <contactoemail.pablo@gmail.com>
2022-09-07 16:19:37 +0200

    Lighthouse: changing thumbnail quality

--- components/molecules/ContentThumbnail.tsx
+++ components/molecules/ContentThumbnail.tsx
@@ -21,7 +21,7 @@ const ContentThumbnail = ({
   const thumbnailImage = `${links.urls.dbImage}/${image}`;
   const thumbnailGenerator = () => (
     <div className="min-w-[181px] min-h-[266.5px] md:min-w-[228px] lg:min-w-[248px] lg:min-h-[362px]  md:min-h-[342px]  cursor-pointer opacity-95 hover:opacity-100 hover:scale-110 transition-all duration-150 ease-in-out hover:saturate-200 flex items-center rounded-md  drop-shadow-md hover:drop-shadow-2xl  ">
-      <ImageAtom src={thumbnailImage} alt={"contentImage"} fill quality={10} />
+      <ImageAtom src={thumbnailImage} alt={"contentImage"} fill quality={1} />
     </div>
   );
   return (
